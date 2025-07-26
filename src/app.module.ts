75f53721407d1ad55122ed46d05260138f59fb22
rogerinfas <rinfas@ulasalle.edu.pe>
2025-07-25 22:05:02 -0500

    conexion a bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '147895326',
+      database: 'nestjs_google_oauth_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Solo para desarrollo, deshabilitar en producción
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
