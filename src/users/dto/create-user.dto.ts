21c45949011eccc607406a0a16cc57fcc8c0ab40
rogerinfas <rinfas@ulasalle.edu.pe>
2025-07-25 22:11:57 -0500

    crud users

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength, IsOptional, IsBoolean } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(6)
+  password?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+}
