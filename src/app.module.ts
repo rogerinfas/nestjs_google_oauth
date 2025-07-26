da989cc4b344a938ba990db43c32bf1febc84f2d
rogerinfas <rinfas@ulasalle.edu.pe>
2025-07-26 01:46:16 -0500

    use .env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,15 +8,20 @@ import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '147895326',
-      database: 'nestjs_google_oauth_db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Solo para desarrollo, deshabilitar en producción
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('_DB_HOST', ''),
+        port: parseInt(config.get<string>('_DB_PORT', '')) || 3306,
+        username: config.get<string>('_DB_USERNAME', ''),
+        password: config.get<string>('_DB_PASSWORD', ''),
+        database: config.get<string>('_DB_NAME', ''),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // Solo para desarrollo, deshabilitar en producción
+      }),
     }),
     UsersModule,
     TasksModule,
